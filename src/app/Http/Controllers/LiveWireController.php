be0b8b6b1ed547afd6ec03ea4db2115d3e65f81b
Katsu <kachan.0627.619@gmail.com>
2020-11-17 22:21:19 +0900

    バージョンアップ

---
+++ src/app/Http/Controllers/LiveWireController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LiveWireController extends Controller
+{
+        //
+        public function index(): void
+        {
+            return view('hello.index',$data);
+        }
+}
